c7a8419c39ff8597dc50e7e19b1ce12826a010ba
Maksym Diachok <maksdyachok2005@gmail.com>
2024-11-13 15:37:18 +0200

    Updates

---
+++ precache-manifest.1562fed1ba1da511120bce7ce4829fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a2cf4917b5b74eb5f5febf8cd5ad7c",
+    "url": "/lab-7-frontend/index.html"
+  },
+  {
+    "revision": "8cd0c1629b17f2032370",
+    "url": "/lab-7-frontend/static/css/main.b9a32696.chunk.css"
+  },
+  {
+    "revision": "2045e704dc7f39d4c912",
+    "url": "/lab-7-frontend/static/js/2.bc929e85.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/lab-7-frontend/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "8cd0c1629b17f2032370",
+    "url": "/lab-7-frontend/static/js/main.e38aac92.chunk.js"
+  },
+  {
+    "revision": "bdd989c9ef3f10eb70a1",
+    "url": "/lab-7-frontend/static/js/runtime~main.0915e982.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BbIXOD/lab-7-frontend/precache-manifest.478c03999c35f673b9776f4f59125658.js"
+  "/lab-7-frontend/precache-manifest.1562fed1ba1da511120bce7ce4829fce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BbIXOD/lab-7-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lab-7-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
