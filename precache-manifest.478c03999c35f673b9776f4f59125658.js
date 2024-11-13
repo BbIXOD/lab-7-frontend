d5010dea9166ec6ad6402d0be3282bcf1b8b4953
Maksym Diachok <maksdyachok2005@gmail.com>
2024-11-13 15:16:10 +0200

    Updates

---
+++ precache-manifest.478c03999c35f673b9776f4f59125658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f2651c39211be8895ea743d8753b50",
+    "url": "/BbIXOD/lab-7-frontend/index.html"
+  },
+  {
+    "revision": "8cd0c1629b17f2032370",
+    "url": "/BbIXOD/lab-7-frontend/static/css/main.b9a32696.chunk.css"
+  },
+  {
+    "revision": "2045e704dc7f39d4c912",
+    "url": "/BbIXOD/lab-7-frontend/static/js/2.bc929e85.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/BbIXOD/lab-7-frontend/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "8cd0c1629b17f2032370",
+    "url": "/BbIXOD/lab-7-frontend/static/js/main.e38aac92.chunk.js"
+  },
+  {
+    "revision": "24726ff9c451cc155a89",
+    "url": "/BbIXOD/lab-7-frontend/static/js/runtime~main.c66d6cd0.js"
+  }
+]);
